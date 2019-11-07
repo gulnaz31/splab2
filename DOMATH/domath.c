0b78e822b2bb541cdec1a080bf55e42dc32b7bdd
gulnaz31 <57069176+gulnaz31@users.noreply.github.com>
2019-11-07 22:24:56 +0600

    Update domath.c

--- DOMATH/domath.c
+++ DOMATH/domath.c
@@ -1,50 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <math.h>
+#include <wait.h>
 
-int main(int argc, char *argv[]){
-	int a,b,result;
-	int pid,pid1,pid2,pid3;
- 
-	if(argc!=3){
-		printf("Invalid args...\n");
-  		return -1;
- }
+int main(int argc, char **argv){
+	int a = atoi(argv[1]);
+	int b = atoi(argv[2]);
 
- a = atoi(argv[1]);
- b = atoi(argv[2]);
-
- pid=fork();
- if(pid == 0) {
-   execlp("python3",":)","add.py",argv[1],argv[2],NULL);
-   exit(0);
- }
-
-else{
-   pid1=fork();
-   if(pid1 == 0){
-    execlp("java",":)","subtraction.java",argv[1],argv[2],NULL);
-    exit(0);
-   }
-
-else{
-  pid2=fork();
-  if(pid2 == 0){
-    execlp("node",":)","multiplication.js",argv[1],argv[2],NULL);
-    exit(0);
-  }
-else{
-  pid3=fork();
-  if(pid3 == 0){
-    execlp("bash",":)","division.sh",argv[1],argv[2],NULL);
-    exit(0);
-  }
-else{ 
-  int rc_wait = waitpid(pid1,0,0);
-  printf("parent: done.\n");
-}
-}
-}
-}
- return 0;
+	pid_t p1 = fork();
+	if(p1==0){
+		printf("child1: %d+%d=%d\n", a, b, a+b);
+	}
+	waitpid(p1, 0, 0);
+	pid_t p2 = fork();
+	if(p2==0 && p1>0){
+		printf("child2: %d-%d=%d\n", a, b, a-b);
+	}
+	waitpid(p2, 0, 0);
+	pid_t p3 = fork();
+	if(p2>0 && p3==0 && p1>0){
+		printf("child3: %d*%d=%d\n", a, b, a*b);
+	}
+	waitpid(p3, 0, 0);
+	pid_t p4 = fork();
+	if(p4==0 && p3>0 && p2>0 && p1>0){
+		printf("child4: %d/%d=%d\n", a, b, a/b);
+	}
+	waitpid(p4, 0, 0);
+	if(p1>0 && p2>0 && p3>0 && p4>0){
+		printf("parent: done.");
+	}
 }
